35f8a5e9a88e7ec4830e220e781dd6a2f4a26755
atrocityz <disbalance17@mail.ru>
2025-12-07 19:28:00 +0700

    feat(frontend): implement animation for apperance of pokemons

--- frontend/src/pages/PokemonsPage/PokemonsPage.tsx
+++ frontend/src/pages/PokemonsPage/PokemonsPage.tsx
@@ -1,43 +1,61 @@
-import { Activity } from "react"
+import { motion } from "motion/react"
+import React, { Activity } from "react"
 
 import { SpinnerIcon } from "@/components/icons"
 import { PokemonCardName, PokemonCardNumber } from "@/components/ui"
 import { formatPokemonId } from "@/utils/helpers"
 
-import { PokemonListSkeleton } from "./components/PokemonListSkeleton"
-import { PokemonPreviewDialog } from "./components/PokemonPreviewDialog"
+import { PokemonListSkeleton } from "./components/PokemonListSkeleton/PokemonListSkeleton"
+import { PokemonPreviewDialog } from "./components/PokemonPreviewDialog/PokemonPreviewDialog"
 import { usePokemonsPage } from "./hooks/usePokemonsPage"
 
 export const PokemonsPage = () => {
   const { state, functions } = usePokemonsPage()
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5 pb-14">
-      {state.isInfiniteQueryLoading ? (
-        <PokemonListSkeleton />
-      ) : (
-        state.pokemons.map((pokemon, index) => {
-          const id = index + 1 >= 1026 ? 9975 + ((index + 1) % 1000) : index + 1
+    <React.Fragment>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5 pb-14">
+        {state.isInfiniteQueryLoading && <PokemonListSkeleton />}
+        {!state.isInfiniteQueryLoading &&
+          state.pokemons.map((pokemon, index) => {
+            const id =
+              index + 1 >= 1026 ? 9975 + ((index + 1) % 1000) : index + 1
 
-          return (
-            <button
-              key={id}
-              aria-label="Open pokemon preview"
-              className="hover:bg-accent relative flex cursor-pointer items-center justify-between gap-2 overflow-hidden rounded-lg border border-zinc-400 px-4 py-3 transition-colors"
-              title="Open pokemon preview"
-              type="button"
-              onClick={() => {
-                functions.selectPokemon(id)
-              }}
-            >
-              <PokemonCardName className="z-1">{pokemon.name}</PokemonCardName>
-              <PokemonCardNumber className="z-1">
-                {formatPokemonId(id)}
-              </PokemonCardNumber>
-            </button>
-          )
-        })
-      )}
+            return (
+              <motion.button
+                key={id}
+                animate={{ opacity: 1, y: 0 }}
+                aria-label="Open pokemon preview"
+                className="hover:bg-accent border-accent-foreground/15 relative flex cursor-pointer items-center justify-between gap-2 overflow-hidden rounded-lg border px-4 py-3 transition-colors"
+                initial={{ opacity: 0, y: -15 }}
+                title="Open pokemon preview"
+                type="button"
+                whileTap={{ scale: 0.99 }}
+                onClick={() => {
+                  functions.selectPokemon(id)
+                }}
+                whileFocus={{ scale: 1.05 }}
+                whileHover={{ scale: 1.05 }}
+              >
+                <PokemonCardName className="z-1">
+                  {pokemon.name}
+                </PokemonCardName>
+                <PokemonCardNumber className="z-1">
+                  {formatPokemonId(id)}
+                </PokemonCardNumber>
+              </motion.button>
+            )
+          })}
+        <Activity mode={state.isFetchingNextPokemonPage ? "visible" : "hidden"}>
+          <PokemonListSkeleton />
+          <div className="col-span-full justify-self-center">
+            <SpinnerIcon className="size-12 animate-spin" />
+          </div>
+        </Activity>
+      </div>
+      <Activity mode={state.isFetchingNextPokemonPage ? "hidden" : "visible"}>
+        <div ref={state.loadMoreRef} className="-mt-[400px]" />
+      </Activity>
       {state.selectedPokemonId && (
         <PokemonPreviewDialog
           isOpen={!!state.selectedPokemonId}
@@ -45,16 +63,6 @@ export const PokemonsPage = () => {
           pokemonId={state.selectedPokemonId}
         />
       )}
-      <Activity mode={state.isFetchingNextPokemonPage ? "visible" : "hidden"}>
-        <PokemonListSkeleton />
-        <div className="col-span-full mx-auto">
-          <SpinnerIcon className="size-12 animate-spin" />
-        </div>
-      </Activity>
-      <Activity mode={state.isFetchingNextPokemonPage ? "hidden" : "visible"}>
-        {/* TODO: Возможно стоит подгружать ref не в конце списка, а заранее */}
-        <div ref={state.loadMoreRef} />
-      </Activity>
-    </div>
+    </React.Fragment>
   )
 }
--- frontend/src/pages/PokemonsPage/components/PokemonListSkeleton/PokemonListSkeleton.tsx
+++ frontend/src/pages/PokemonsPage/components/PokemonListSkeleton/PokemonListSkeleton.tsx
@@ -1,14 +1,16 @@
 import {
-  PokemonCard,
   PokemonCardSkeletonName,
   PokemonCardSkeletonNumber,
 } from "@/components/ui"
-import { POKEMONS_QUERY } from "@/utils/constants"
+import { POKEMONS } from "@/utils/constants"
 
 export const PokemonListSkeleton = () =>
-  Array.from({ length: POKEMONS_QUERY.LIMIT }).map((_, index) => (
-    <PokemonCard key={index} className="flex justify-between gap-4">
+  Array.from({ length: POKEMONS.LIMIT }).map((_, index) => (
+    <div
+      key={index}
+      className="border-accent-foreground/15 flex items-center justify-between gap-2 overflow-hidden rounded-lg border px-4 py-3"
+    >
       <PokemonCardSkeletonName />
       <PokemonCardSkeletonNumber />
-    </PokemonCard>
+    </div>
   ))
