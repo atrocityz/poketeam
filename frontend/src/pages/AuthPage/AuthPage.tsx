2d15c2e16e57c2e459c99b14162ef6062b610c21
atrocityz <disbalance17@mail.ru>
2026-01-14 14:53:54 +0700

    fix(frontend): correct auth page height on mobile

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -2,7 +2,7 @@ import { FormContainer } from "./components/FormContainer/FormContainer"
 import { ThemeToggler } from "./components/ThemeToggler"
 
 export const AuthPage = () => (
-  <div className="mx-auto flex min-h-screen items-center justify-center px-4">
+  <div className="mx-auto flex min-h-dvh items-center justify-center px-4">
     <div className="fixed top-5 right-5">
       <ThemeToggler />
     </div>
